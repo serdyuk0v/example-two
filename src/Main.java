753e82477d8eed530f2d13c405162a94da4af6d9
Serdyukov <and.serdykov@gmail.com>
2022-08-20 19:49:26 +0300

    Task one

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Example Two - Variables");
+        //Int
+        var year = 2022;
+        byte typeByte = 127; //1 byte
+        short typeShort = 32_767; //2 byte
+        int typeInt = 2_147_483_647; //4 byte
+        long typeLong = 9_223_372_036_854_775_807L; //8 byte
+
+        //Float
+        float typeFloat = 3.1415926F; //4 byte
+        double typeDouble = 3.14159265358979323846; //8 byte
+
+        //Char
+        char typeChar = 227; //2 byte
+
+        //Boolean
+        boolean typeBoolean = year > 2021; // 1 bit
+
     }
 }
