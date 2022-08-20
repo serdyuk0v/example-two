aac18df53d26cc19a4b120743780950a45865e52
Serdyukov <and.serdykov@gmail.com>
2022-08-20 20:28:27 +0300

    Task three

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Example Two - Variables");
-
+        System.out.println("Example Two - Переменные");
         //Task One
+        System.out.println("Задача 1");
         //Int
         var year = 2022;
         byte typeByte = 127; //1 byte
@@ -17,12 +17,26 @@ public class Main {
         //Boolean
         boolean typeBoolean = year > 2021; // 1 bit
 
+        System.out.println("******************************************");
         //Task Two
+        System.out.println("Задача 2");
         float fighterOne = 78.2F;
         float fighterTwo = 82.8F;
         float totalWeightFighter = fighterOne + fighterTwo;
         float diffWeightFighter = fighterOne - fighterTwo;
         System.out.println("Общий вес бойцов: " + totalWeightFighter + " кг");
-        System.out.println("Разница в весе: " + diffWeightFighter + " кг");
+        System.out.println("Разница в весе бойцов: " + diffWeightFighter + " кг");
+
+        System.out.println("******************************************");
+        //Task 3
+        System.out.println("Задача 3");
+        int bananas = 5 * 80;
+        int milk = 105;
+        int iceCream = 2 * 100;
+        int eggs = 4 * 70;
+        int blender = bananas + milk + iceCream + eggs;
+        System.out.println("Вес в блендере: " + blender + " грамм");
+        float weightBreakfast = blender / 1000F;
+        System.out.println("Общий вес завтрака спортсмена: " + weightBreakfast + " кг");
     }
 }
